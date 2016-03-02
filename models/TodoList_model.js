89d059f8a23d5b3c830b9b35dc68b575f68e948b
Priyesh Gohil <priyesh114@hotmail.co.uk>
2016-03-02 22:17:25 +0000

    Added custom method to create timestamps

--- models/TodoList_model.js
+++ models/TodoList_model.js
@@ -4,9 +4,18 @@ var Schema = mongoose.Schema;
 
 var todoListSchema = new Schema( {
   task      : String,
-  completed : {type: Boolean,  default:false},
+  completed : {type: Boolean, default:false},
   created_at: Date,
   update_at : Date
+});
+
+// custom method to add current Date
+todoListSchema.pre('save', function(next) {
+  var currentDate = new Date();
+  this.update_at = currentDate;
+
+  if(!this.created_at) this.created_at = currentDate;
+  next();
 });
 
 module.exports = mongoose.model('TodoList',todoListSchema);
